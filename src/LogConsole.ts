85784db751789c66711a6cde9573a71c0e32d222
Marcos Palacios <m.palacios@geekshubs.com>
2020-12-07 19:47:06 +0100

    configure github actions

---
+++ src/LogConsole.ts
@@ -0,0 +1,12 @@
+class LogConsole {
+    constructor(){
+        this.print();
+    }
+    public print(): void {
+        console.log("Hola mundo");
+    }
+}
+
+export default LogConsole;
+
+console.log("Hola mundo");
